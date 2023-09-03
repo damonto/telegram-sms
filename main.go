991da0019587158d42eb9f2c418d17d0951da418
Damon To <damon.tu@outlook.com>
2023-09-03 18:29:52 +0800

    chore: code improvements

--- main.go
+++ main.go
@@ -64,7 +64,7 @@ func main() {
 				slog.Info("command received", "command", update.Message.Command(), "raw", update.Message.Text)
 				if err := handler.HandleCommand(update.Message.Command(), update.Message); err != nil {
 					slog.Error("failed to handle command", "error", err)
-					msg := tgbotapi.NewMessage(chatId, err.Error())
+					msg := tgbotapi.NewMessage(chatId, escapeText(err.Error()))
 					if _, err := bot.Send(msg); err != nil {
 						slog.Error("failed to send message", "text", msg.Text, "error", err)
 					}
@@ -76,7 +76,7 @@ func main() {
 				slog.Info("command callback received", "button", update.CallbackQuery.Data)
 				if err := handler.HandleCallback(update.CallbackQuery); err != nil {
 					slog.Error("failed to handle callback", "error", err)
-					msg := tgbotapi.NewMessage(chatId, err.Error())
+					msg := tgbotapi.NewMessage(chatId, escapeText(err.Error()))
 					if _, err := bot.Send(msg); err != nil {
 						slog.Error("failed to send message", "text", msg.Text, "error", err)
 					}
@@ -105,7 +105,7 @@ func main() {
 			return
 		}
 
-		msg := tgbotapi.NewMessage(chatId, fmt.Sprintf("*\\[%s\\] %s*\n%s", operator, escapeText(sender), escapeText(text)))
+		msg := tgbotapi.NewMessage(chatId, escapeText(fmt.Sprintf("*[%s] %s*\n%s", operator, sender, text)))
 		msg.ParseMode = "markdownV2"
 		if _, err := bot.Send(msg); err != nil {
 			slog.Error("failed to send message", "text", msg.Text, "error", err)
