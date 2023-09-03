93da0480442282b9181418f24fd0be82d49ee9fa
Damon To <damon.tu@outlook.com>
2023-09-03 13:57:05 +0800

    chore: change build output binary name

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -o build/linux-amd64-sms *.go
-chmod +x build/linux-amd64-sms
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -o build/linux-amd64-telegram-sms *.go
+chmod +x build/linux-amd64-telegram-sms
 
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -trimpath -o build/linux-arm64-sms *.go
-chmod +x build/linux-arm64-sms
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -trimpath -o build/linux-arm64-telegram-sms *.go
+chmod +x build/linux-arm64-telegram-sms
