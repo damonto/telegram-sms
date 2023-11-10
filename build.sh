7291653acd25846ff8f1139612292d6f821f3586
Damon To <damon.tu@outlook.com>
2023-11-10 23:02:02 +0800

    chore: remove build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-w -s" -o build/linux-amd64-telegram-sms *.go
-chmod +x build/linux-amd64-telegram-sms
-
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -trimpath -ldflags="-w -s" -o build/linux-arm64-telegram-sms *.go
-chmod +x build/linux-arm64-telegram-sms
