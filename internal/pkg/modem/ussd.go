b523acc871d4a5f5fe65fee704e60a4fc0e59fd1
Damon To <damon.tu@outlook.com>
2024-06-17 21:10:25 +0800

    refactor: cancel ussd session after sending failure

--- internal/pkg/modem/ussd.go
+++ internal/pkg/modem/ussd.go
@@ -1,16 +1,24 @@
 package modem
 
+import "log/slog"
+
 func (m *Modem) RunUSSDCommand(command string) (string, error) {
 	three3gpp, err := m.modem.Get3gpp()
 	if err != nil {
 		return "", err
 	}
-
 	ussd, err := three3gpp.GetUssd()
 	if err != nil {
 		return "", err
 	}
-	return ussd.Initiate(command)
+	reply, err := ussd.Initiate(command)
+	if err != nil {
+		if err := ussd.Cancel(); err != nil {
+			slog.Error("failed to cancel ussd command", "error", err)
+		}
+		return "", err
+	}
+	return reply, nil
 }
 
 func (m *Modem) RespondUSSDCommand(response string) (string, error) {
@@ -18,10 +26,16 @@ func (m *Modem) RespondUSSDCommand(response string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	ussd, err := three3gpp.GetUssd()
 	if err != nil {
 		return "", err
 	}
-	return ussd.Respond(response)
+	reply, err := ussd.Respond(response)
+	if err != nil {
+		if err := ussd.Cancel(); err != nil {
+			slog.Error("failed to cancel ussd command", "error", err)
+		}
+		return "", err
+	}
+	return reply, nil
 }
