7351282dd53c113f4671fd2fc354f2068c5c9c38
Damon To <damon.tu@outlook.com>
2024-09-12 14:31:23 +0800

    fix(ci): Update go generate step and fix curl command i

--- internal/pkg/util/carrier.go
+++ internal/pkg/util/carrier.go
@@ -1,6 +1,6 @@
 package util
 
-//go:generate sh -c "curl -L -o carrier_list.textpb https://android.googlesource.com/platform/packages/providers/TelephonyProvider/+/main/assets/latest_carrier_id/carrier_list.textpb?format=text"
+//go:generate url -L -o carrier_list.textpb https://android.googlesource.com/platform/packages/providers/TelephonyProvider/+/main/assets/latest_carrier_id/carrier_list.textpb?format=text
 
 import (
 	_ "embed"
