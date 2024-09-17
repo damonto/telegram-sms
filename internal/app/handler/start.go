cb898122f6d4d5e16d422c22a0bd3f8f75456db3
Damon To <damon.tu@outlook.com>
2024-09-17 13:37:05 +0800

    fix(handler): escape user names in start message

--- internal/app/handler/start.go
+++ internal/app/handler/start.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 
+	"github.com/damonto/telegram-sms/internal/pkg/util"
 	"gopkg.in/telebot.v3"
 )
 
@@ -14,7 +15,7 @@ Hello, *%s %s*\!
 Thanks for using this bot\.
 Your UID is *%d*
 	`
-	return c.Send(fmt.Sprintf(message, c.Sender().FirstName, c.Sender().LastName, c.Sender().ID), &telebot.SendOptions{
+	return c.Send(fmt.Sprintf(message, util.EscapeText(c.Sender().FirstName), util.EscapeText(c.Sender().LastName), c.Sender().ID), &telebot.SendOptions{
 		ParseMode: telebot.ModeMarkdownV2,
 	})
 }
