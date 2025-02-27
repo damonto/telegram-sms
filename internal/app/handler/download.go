a112fcbfefd5ee95d0f819204eb7cf0b111074f8
Damon To <damon.tu@outlook.com>
2025-02-27 21:16:39 +0800

    feat(wip): add support for downloading eSIM profile

--- internal/app/handler/download.go
+++ internal/app/handler/download.go
@@ -1,17 +1,20 @@
 package handler
 
 import (
-	"fmt"
+	"net/url"
+	"strings"
 
 	"github.com/damonto/euicc-go/lpa"
 	"github.com/damonto/telegram-sms/internal/app/state"
 	"github.com/damonto/telegram-sms/internal/pkg/modem"
+	"github.com/damonto/telegram-sms/internal/pkg/util"
 	"github.com/mymmrac/telego"
 	th "github.com/mymmrac/telego/telegohandler"
 )
 
 type DownloadHandler struct {
 	*Handler
+	cc chan string
 }
 
 type DownloadValue struct {
@@ -27,18 +30,65 @@ const (
 
 func NewDownloadHandler() state.Handler {
 	h := new(DownloadHandler)
+	h.cc = make(chan string, 1)
 	return h
 }
 
 func (h *DownloadHandler) Handle() th.Handler {
 	return func(ctx *th.Context, update telego.Update) error {
-		fmt.Println("DownloadHandler Handle")
-		return nil
+		m := h.Modem(ctx)
+		state.M.Enter(update.Message.Chat.ID, &state.ChatState{
+			Handler: h,
+			State:   DownloadAskActivationCode,
+			Value:   &DownloadValue{Modem: m},
+		})
+		_, err := h.Reply(ctx, update, "Please enter the activation code", nil)
+		return err
 	}
 }
 
 func (h *DownloadHandler) HandleMessage(ctx *th.Context, message telego.Message, s *state.ChatState) error {
+	value := s.Value.(*DownloadValue)
+	if s.State == DownloadAskActivationCode {
+		return h.downloadProfile(ctx, message, s, value)
+	}
 	return nil
+}
+
+func (h *DownloadHandler) downloadProfile(ctx *th.Context, message telego.Message, s *state.ChatState, value *DownloadValue) error {
+	ac, ccRequired, err := h.parseActivationCode(message.Text)
+	if err != nil {
+		return err
+	}
+	value.ActvationCode = ac
+	if ccRequired {
+		state.M.Current(message.From.ID, DownloadAskConfirmationCode)
+		_, err := h.ReplyMessage(ctx, message, util.EscapeText("Please enter the confirmation code"), nil)
+		return err
+	}
+	return h.download(ctx, message, s, value)
+}
+
+func (h *DownloadHandler) download(ctx *th.Context, message telego.Message, s *state.ChatState, value *DownloadValue) error {
+	return nil
+}
+
+// LPA:1$
+func (h *DownloadHandler) parseActivationCode(text string) (ac *lpa.ActivationCode, ccRequired bool, err error) {
+	parts := strings.Split(text, "$")
+	ac = &lpa.ActivationCode{
+		SMDP: &url.URL{Scheme: "https", Host: parts[1]},
+		IMEI: "123456789012345",
+	}
+	if len(parts) == 3 {
+		ac.MatchingID = parts[2]
+	}
+	if len(parts) == 5 {
+		if parts[4] == "1" {
+			ccRequired = true
+		}
+	}
+	return ac, ccRequired, nil
 }
 
 func (h *DownloadHandler) HandleCallbackQuery(ctx *th.Context, query telego.CallbackQuery, s *state.ChatState) error {
