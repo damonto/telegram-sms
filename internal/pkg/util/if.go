1b3dfc9faf6a5794a9ce1e3a4148c41a06385c16
Damon To <damon.tu@outlook.com>
2025-03-20 14:38:59 +0800

    fix: simplify If function by using 'any' type instead of IfValue interface

--- internal/pkg/util/if.go
+++ internal/pkg/util/if.go
@@ -1,13 +1,6 @@
 package util
 
-type IfValue interface {
-	~int | ~int8 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint32 | ~uint64 |
-		~float32 | ~float64 | ~string | ~bool |
-		~[]byte | ~[]rune
-}
-
-func If[T IfValue](condition bool, trueVal T, falseVal T) T {
+func If[T any](condition bool, trueVal T, falseVal T) T {
 	if condition {
 		return trueVal
 	}
